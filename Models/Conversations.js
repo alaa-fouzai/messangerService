09ccbceea5aff53e6b0b8bfd3c05d8c75c8c555f
alaa-fouzai1 <alaa.fouzai@esprit.tn>
2023-05-04 16:58:34 +0100

    messages

---
+++ Models/Conversations.js
@@ -0,0 +1,40 @@
+const { ObjectId } = require('mongoose');
+const mongoose=require('mongoose');
+
+const ChatSchema = mongoose.Schema(
+    {
+        id: {
+            type : String,
+            required : false
+        },
+        Name: {
+            type : String,
+            required : true
+        },
+        email: {
+            type : String,
+            required : true
+        },
+        adminId:[Object],
+        Created_date: {
+            type : Date,
+            default : Date.now()
+        },
+        ParentChatId:{
+            type : String,
+            required : true
+        },
+        state : {
+            type :Boolean,
+            default:true
+        },
+        Property:{
+            type : String,
+            required : false
+        }
+        ,
+        texts: [Object]
+    }
+);
+
+module.exports=mongoose.model('Conversations',ChatSchema);
